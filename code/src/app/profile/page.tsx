f30231f4561db3eb09f3d8bb595874ff5673d226
kacieyee <kacieyee@gmail.com>
2025-03-13 15:38:03 -0500

    started working on the profile page

--- code/src/app/profile/page.tsx
+++ code/src/app/profile/page.tsx
@@ -1,8 +1,20 @@
+import './profile.css';
+
 export default function Profile() {
     return (
-      <div className="flex flex-col h-screen p-6">
-        <h1 className="text-4xl font-bold">Profile</h1>
-        <p className="mt-4 text-lg">Welcome to your profile!</p>
+      <div className="row">
+        <div className="left"> 
+          <img src="https://pbs.twimg.com/media/GLhxXtfXQAA0YbG?format=jpg&name=medium" alt="profile picture"></img>
+          
+          <div className="profileSection">
+            <p>Name: Super Cool Team Leader Gabby</p>
+            <br></br>
+            <p>Password: **************</p>
+          </div>
+        </div>
+        <div className="right">
+          <h1>Budgeting Goals</h1>
+        </div>
       </div>
     );
   }
