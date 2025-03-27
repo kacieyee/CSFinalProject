51bac8ac50743cde501a54b5144c6908a6dcd350
Viet-Vu <thevuviet01@gmail.com>
2025-03-27 11:42:19 -0500

    added delete features, polished other pages, and began budgeting data

--- code/src/app/api/transactions/route.js
+++ code/src/app/api/transactions/route.js
@@ -3,7 +3,7 @@ import User from "@/models/User";
 import { NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 
-export async function postTransaction(request) {
+export async function POST(request) {
     try {
         const {token, name, price, date, vendor, category} = await request.json();
 
@@ -37,10 +37,6 @@ export async function postTransaction(request) {
     }
 }
 
-export async function POST(request) {
-    return postTransaction(request);
-}
-
 export async function GET(request) {
     try {
         const token = request.headers.get("Authorization")?.split(" ")[1];
@@ -57,3 +53,31 @@ export async function GET(request) {
         return NextResponse.json({message: "Error fetching transactions"}, {status: 500});
     }
 }
+
+export async function DELETE(request) {
+    try {
+      const {transactionId} = await request.json();
+      const token = request.headers.get("Authorization")?.split(" ")[1];
+  
+      if (!token) return NextResponse.json({message: "Unauthorized"}, {status: 401});
+  
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const user = await User.findOne({username: decoded.username});
+  
+      if (!user) return NextResponse.json({message: "User not found"}, {status: 404});
+  
+      const transactionIndex = user.transactions.findIndex(txn => txn._id.toString() === transactionId);
+      if (transactionIndex === -1) {
+        return NextResponse.json({message: "Transaction not found"}, {status: 404});
+      }
+  
+      user.transactions.splice(transactionIndex, 1);
+      await user.save();
+  
+      return NextResponse.json({message: "Transaction deleted successfully"}, {status: 200});
+    } catch (error) {
+      console.error(error);
+      return NextResponse.json({message: "Error deleting transaction"}, {status: 500});
+    }
+  }
+  
