1f1da21b128081f87028a60e1b8ba8fcbbf64a76
Gabrielle Kuruvilla <gabrielle.kuruvilla@gmail.com>
2025-04-25 01:40:55 -0500

    temp line

--- code/src/app/home/page.tsx
+++ code/src/app/home/page.tsx
@@ -1,16 +1,38 @@
+"use client";
 import Link from "next/link";
 import pinkLogo from "../../assets/pink logo.png";
 import Image from "next/image";
 import "./home.css";
+import * as turf from "@turf/turf";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [linePoints, setLinePoints] = useState("");
+
+  useEffect(() => {
+    const points = [
+      [0, 60],
+      [200, 50],
+      [400, 70],
+      [600, 40],
+      [800, 60],
+      [1000, 55],
+    ];
+
+    const lineString = turf.lineString(points);
+    const curve = turf.bezierSpline(lineString, { resolution: 1000 });
+    const coordinates = curve.geometry.coordinates;
+    const svgPoints = coordinates.map(coord => coord.join(",")).join(" ");
+    setLinePoints(svgPoints);
+  }, []);
+
   return (
     <div className="container">
-              <Image 
-                src={pinkLogo} 
-                alt="Pink Logo" 
-                className="logo"
-              />
+      <Image 
+        src={pinkLogo}
+        alt="Pink Logo"
+        className="logo" 
+      />
       <h1 className="heading">
         Reaching our budgeting goals together.<br />
         For the girlies, by the girlies.
@@ -20,14 +42,34 @@ export default function Home() {
         <Link href="/login">
           <button className="pink-button">
             Login
-          </button>
+            </button>
         </Link>
         <Link href="/signup">
           <button className="pink-button">
             Sign Up
-          </button>
+            </button>
         </Link>
       </div>
+
+      <svg
+        viewBox="0 0 1000 100"
+        preserveAspectRatio="none"
+        style={{
+          position: "absolute",
+          bottom: 0,
+          left: 0,
+          width: "100%",
+          height: "80px",
+          zIndex: -1,
+        }}
+      >
+        <polyline
+          points={linePoints}
+          fill="none"
+          stroke="#FF9BD1"
+          strokeWidth="8"
+        />
+      </svg>
     </div>
   );
 }
